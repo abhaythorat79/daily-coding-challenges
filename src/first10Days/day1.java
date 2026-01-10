d213b7d0176bb054019f3e5ef5b3793158f0dd31
Abhay Thorat <abhaythorat79@gmail.com>
2026-01-10 15:11:50 +0800

    Day 02

--- src/first10Days/day1.java
+++ src/first10Days/day1.java
@@ -1,6 +1,6 @@
-package day1;
+package first10Days;
 
-public class HelloWorld {
+public class day1 {
     public static void main(String[] args) {
         System.out.println("Day 01 - Daily Coding Challenge Started Abhay!");
     }
---
+++ src/first10Days/day2.java
@@ -0,0 +1,19 @@
+package first10Days;
+//Conditions
+//Take one integer input
+//
+//If the number is divisible by 2 → print Even
+//
+//Otherwise → print Odd
+
+public class day2 {
+    public static void main(String[] args) {
+        int n = 6;
+
+        if (n%2==0) {
+            System.out.print("Is Even number");
+        } else {
+            System.out.print("Is odd number");
+        }
+    }
+}
