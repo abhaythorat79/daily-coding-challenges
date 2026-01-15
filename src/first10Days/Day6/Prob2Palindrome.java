9cb46662d29283402f6eabf2d396613790af3b9f
Abhay Thorat <abhaythorat79@gmail.com>
2026-01-16 06:32:09 +0800

    Initial commit

--- src/first10Days/Day6/Prob2Palindrome.java
+++ src/first10Days/Day6/Prob2Palindrome.java
@@ -2,17 +2,14 @@ package first10Days.Day6;
 
 public class Prob2Palindrome {
     public static void main(String[] args) {
-        int n = 121;
-        int temp=n;
+        int n = 121,temp=n;
         int reverse = 0;
         while (n > 0) {
             int digit = n % 10;
             reverse = reverse * 10 + digit;
             n = n / 10;
-        }
-        if (reverse == temp) {
+        }if (reverse == temp) {
             System.out.println("Palindrome");
-
         }else {
             System.out.println("Not Palindrome");
         }
