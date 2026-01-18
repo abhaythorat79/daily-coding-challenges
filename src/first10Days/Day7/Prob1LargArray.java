73e50ffb5fdc355544aa03bcf7dfac23412e356f
Abhay Thorat <abhaythorat79@gmail.com>
2026-01-18 19:18:31 +0800

    Initial commit

---
+++ src/first10Days/Day7/Prob1LargArray.java
@@ -0,0 +1,11 @@
+package first10Days.Day7;
+
+public class Prob1LargArray {
+    public static void main(String[] args){
+        int [] a={10,20,80,90,40};
+        int temp= 0;
+        for(int i=0;i<a.length;i++){
+            temp=a[i];
+        }
+    }
+}
