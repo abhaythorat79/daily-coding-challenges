11849af728990a99377c4324a7b5451e021c1a3b
Abhay Thorat <abhaythorat79@gmail.com>
2026-01-19 21:59:05 +0800

    EDay 7

--- src/first10Days/Day7/Prob1LargArray.java
+++ src/first10Days/Day7/Prob1LargArray.java
@@ -1,11 +1,21 @@
 package first10Days.Day7;
 
 public class Prob1LargArray {
-    public static void main(String[] args){
-        int [] a={10,20,80,90,40};
-        int temp= 0;
-        for(int i=0;i<a.length;i++){
-            temp=a[i];
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50};
+        int sum = 0;
+        int max = a[0];
+        // Print elements, calculate sum and max
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+            sum += a[i];
+            if (a[i] > max) {
+                max = a[i];
+            }
         }
+        System.out.println(); // new line
+        System.out.println("Sum = " + sum);
+        System.out.println("Max = " + max);
     }
 }
+
