037a6b47806e5871adcedd56ec1dac5fefc8c91b
Abhay Thorat <abhaythorat79@gmail.com>
2026-01-25 09:32:15 +0800

    Day 9

---
+++ src/first10Days/Day9/Stringovel.java
@@ -0,0 +1,15 @@
+package first10Days.Day9;
+
+public class Stringovel {
+    public static void main(String[] args){
+        String s="education";
+        int count=0;
+        for(int i=0; i< s.length();i++){
+            char ch=s.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
