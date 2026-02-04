789d063468ea924903042d45369d4ce0c00b7e35
Abhay Thorat <abhaythorat79@gmail.com>
2026-02-04 20:55:02 +0800

    Dsa day10

---
+++ src/first10Days/ArrayProb/ReverseEachWord.java
@@ -0,0 +1,24 @@
+package first10Days.ArrayProb;
+
+public class ReverseEachWord {
+    public static void main(String[] args) {
+
+        String s = "Abhay Gokul Thorat is a good programmer";
+        String[] words = s.split(" ");
+
+        String result = "";
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            String revWord = "";
+
+            for (int j = word.length() - 1; j >= 0; j--) {
+                revWord = revWord + word.charAt(j);
+            }
+
+            result = result + revWord + " ";
+        }
+
+        System.out.println(result.trim());
+    }
+}
