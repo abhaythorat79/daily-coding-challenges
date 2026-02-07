9f240e950efb0cb508a25912a73c360772ad001c
Abhay Thorat <abhaythorat79@gmail.com>
2026-02-07 23:11:52 +0800

    Dsa day11

--- src/first10Days/Day9Strings/FirstNonRepeatingChar.java
+++ src/first10Days/Day9Strings/FirstNonRepeatingChar.java
@@ -1,4 +1,26 @@
 package first10Days.Day9Strings;
 
 public class FirstNonRepeatingChar {
+    public static void main(String[] args) {
+
+        String s = "swiss";
+        s = s.toLowerCase();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int count = 0;
+
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == ch) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println(ch);
+                break;
+            }
+        }
+    }
 }
+
