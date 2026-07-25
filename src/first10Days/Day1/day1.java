2ca9f1d4d3b07d414cad7be3bc104accc07bd4b8
Abhay Thorat <abhaythorat79@gmail.com>
2026-07-25 23:41:45 +0530

    normal

--- src/first10Days/Day1/day1.java
+++
@@ -1,8 +0,0 @@
-package first10Days.Day1;
-
-public class day1 {
-    public static void main(String[] args) {
-        System.out.println("Day 01 - Daily Coding Challenge Started Abhay!");
-    }
-
-}
